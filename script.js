2f2c5246c4a24f3e2c3d287ee01122c39f5465ce
Abraomukas <abraomukas@gmail.com>
2023-05-25 09:40:10 +0400

    wip: HubSpot API integration

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 const xlsx = require('xlsx');
+const axios = require('axios');
+const { config } = require('dotenv');
 
-const fileLocation = './HubpotDB.xlsx';
+require('dotenv').config();
+
+const fileLocation = process.env.FILE_LOCATION;
 const file = xlsx.readFile(fileLocation);
 
 const sheetName = file.SheetNames[0];
 const sheet = file.Sheets[sheetName];
 
-const json = xlsx.utils.sheet_to_json(sheet, { header: 1 });
+const jsonArray = xlsx.utils.sheet_to_json(sheet, { header: 1 });
 
-console.log(json);
+jsonArray.forEach((json, count) => {
+	console.log('Element #' + count);
+	console.log(json);
+});
+
+//TODO Send every element to HubSpot
+// const endpoint = '/crm/v3/objects/';
+const method = 'POST';
+const apiUrl = process.env.HUBSPOT_API_URL;
+const apiKey = process.env.HUBSPOT_API_KEY;
+const url = apiUrl + `${endpoint}?hapikey=` + apiKey;
+const headers = { 'Content-Type': 'application/json' };
+
+// const config = { method, url, headers, jsonArray };
+axios(config)
+	.then((res) => {
+		console.log('Data sent to HubSpot!', res.data);
+	})
+	.catch((err) => {
+		console.log('ERROR: Sending data to HubSpot failed!', err);
+	});
+    
+//TODO Use @hubspot/api-client
